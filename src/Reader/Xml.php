a914505a46a3de61577f2f9b91ce251910e51037
zf2timo <zf2timo@users.noreply.github.com>
2016-03-23 11:14:35 +0100

    Removed bool as return Value for fromString

--- src/Reader/Xml.php
+++ src/Reader/Xml.php
@@ -85,7 +85,7 @@ class Xml implements ReaderInterface
      *
      * @see    ReaderInterface::fromString()
      * @param  string $string
-     * @return array|bool
+     * @return array
      * @throws Exception\RuntimeException
      */
     public function fromString($string)
