190ffd8672c1af26df594639c038d0b14b198c6e
Matthew Weier O'Phinney <matthew@zend.com>
2017-02-16 14:28:58 -0600

    Added license-check to QA toolchain
    - Now require `malukehno/docheader` in development.
    - Added configuration via `.docheader`.
    - `license-check` will check all `src/` files.
    - `license-check-all` will check all `src/` and `test/` files; there are
      test assets that are _expected_ to fail.
    - Travis runs `license-check` for targets doing CS checks.

--- test/AbstractConfigFactoryTest.php
+++ test/AbstractConfigFactoryTest.php
@@ -1,12 +1,9 @@
 <?php
 
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @author    Chris Raidler <chris@raidler.com>
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/zendframework/zend-config for the canonical source repository
+ * @copyright Copyright (c) 2005-2017 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   https://github.com/zendframework/zend-config/blob/master/LICENSE.md New BSD License
  */
 
 namespace ZendTest\Config;
