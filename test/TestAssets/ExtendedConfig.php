f5fffffca60af19a770d7986e138e2238d397709
webimpress <contact@webimpress.com>
2019-01-29 20:52:04 +0000

    ExtendedConfig class which extends Zend\Config\Config - tests

---
+++ test/TestAssets/ExtendedConfig.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @see       https://github.com/zendframework/zend-config for the canonical source repository
+ * @copyright Copyright (c) 2019 Zend Technologies USA Inc. (https://www.zend.com)
+ * @license   https://github.com/zendframework/zend-config/blob/master/LICENSE.md New BSD License
+ */
+
+namespace ZendTest\Config\TestAssets;
+
+use Zend\Config\Config;
+
+class ExtendedConfig extends Config
+{
+}
